7ff5343c2b6ee48441431f0c9c2138a386f95417
hafsaidiousse <hafsaidiousse@gmail.com>
2023-05-21 17:33:38 -0700

    task 1

--- simple_shell.c
+++ simple_shell.c
@@ -5,53 +5,81 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <string.h>
 
+/**
+ * get_cmd - get cmd from line
+ * @line: line
+ * Return: cmd
+ */
 char *get_cmd(char *line)
 {
-	int len = 0;
-	char *cmd;
-	int i = 0;
+    int len = 0;
+    char *cmd;
+    int i = 0;
 
-	while(line[i] != '\0' && line[i] != ' ' && line[i] != '\n')
-	{
-		i++;
-		len++;
-	}
-	cmd = (char *) malloc(len * sizeof(char));
-	if (cmd == NULL)
-		return (NULL);
-	for (i = 0; i < len; i++)
-		cmd[i] = line[i];
+    while (line[i] != '\0' && line[i] != ' ' && line[i] != '\n')
+    {
+        i++;
+        len++;
+    }
+    cmd = (char *)malloc((len + 1) * sizeof(char));
+    if (cmd == NULL)
+        return (NULL);
+    for (i = 0; i < len; i++)
+        cmd[i] = line[i];
+    cmd[len] = '\0';
 
-	return (cmd);
+    return (cmd);
 }
+
+/**
+ * get_argv - split line into argv
+ * @line: line
+ * Return: argv
+ */
+#include <string.h>
 
 char **get_argv(char *line)
 {
-	int nt = 0;
-	int i = 0;
-	int j = 0;
-	int len = 0;
-	char **av;
+    char **argv;
+    int i = 0;
+    int j = 0;
+    int len = 0;
+    int words = 0;
 
-	while (line[i] != '\0')
-	{
-		if (line[i] == ' ')
-			nt++;
-		i++;
-	}
-	nt++;
-	av =  (char **) malloc(sizeof(char *) * nt);
-	if (av == NULL)
-		return (NULL);
-	i = 0;
-	while (line[i] != '\0')
-	{
-		len 
+    while (line[i] != '\0')
+    {
+        if (line[i] == ' ')
+            words++;
+        i++;
+    }
+    words++;
+    argv = (char **)malloc((words + 1) * sizeof(char *));
+    if (argv == NULL)
+        return (NULL);
 
+    for (i = 0; i < words; i++)
+    {
+        while (line[j] != '\0' && line[j] != ' ' && line[j] != '\n')
+        {
+            len++;
+            j++;
+        }
+        argv[i] = (char *)malloc((len + 1) * sizeof(char));
+        if (argv[i] == NULL)
+            return (NULL);
 
-	}
+        strncpy(argv[i], &line[j - len], len);
+        argv[i][len] = '\0';
 
+        len = 0;
+        while (line[j] == ' ' || line[j] == '\n')
+            j++;
+    }
+
+    argv[i] = NULL;
+    return argv;
 }
 
 /**
@@ -64,47 +92,52 @@ char **get_argv(char *line)
  */
 int main(int argc, char **argv, char **envp)
 {
-	char *line;
-	char *cmd;
-	char **av;
-	size_t len = 0;
-	ssize_t read;
-	pid_t pid;
-	int status;
+    char *line;
+    char *cmd;
+    int i;
+    char **av;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+    int status;
 
-	(void) argc;
-	(void) av;
+    (void)argc;
+    (void)argv;
 
-	while (1)
-	{
-		write(STDOUT_FILENO, "#cisfun$ ", 9);
-		fflush(stdout);
-		read = getline(&line, &len, stdin);
-		if (read != -1)
-		{
-			cmd = get_cmd(line);
-			pid = fork();
-			if (pid < 0)
-			{
-				write(STDERR_FILENO, "Fork Failed", 11);
-				return (1);
-			}
-			else if (pid == 0)
-			{
-				if (execve(cmd, argv, envp) == -1)
-					perror("./shell");
-				_exit(0);
-			}
-			else
-				wait(&status);
-		}
-		else
-		{
-			write(STDOUT_FILENO, "\n", 1);
-			exit(0);
-		}
-	}
-	free(line);
-	free(cmd);
-	return (0);
+    while (1)
+    {
+        write(STDOUT_FILENO, "#cisfun$ ", 9);
+        fflush(stdout);
+        read = getline(&line, &len, stdin);
+        if (read != -1)
+        {
+            cmd = get_cmd(line);
+            av = get_argv(line);
+            pid = fork();
+            if (pid < 0)
+            {
+                write(STDERR_FILENO, "Fork Failed", 11);
+                return (1);
+            }
+            else if (pid == 0)
+            {
+                if (execve(cmd, av, envp) == -1)
+                    perror("./shell");
+                _exit(0);
+            }
+            else
+                wait(&status);
+        }
+        else
+        {
+            write(STDOUT_FILENO, "\n", 1);
+            exit(0);
+        }
+        free(line);
+        free(cmd);
+        for (i = 0; av[i] != NULL; i++)
+            free(av[i]);
+        free(av);
+    }
+    return (0);
 }
